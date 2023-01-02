2deea5ed8e1ae0af9a0254d037c05bbcbcd90ee8
Ivan Petrov <lazytechwork@gmail.com>
2023-01-02 18:38:13 +0300

    Removed handling exceptions in index.php

--- public/index.php
+++ public/index.php
@@ -2,18 +2,8 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Yumi\Application;
 
 require __DIR__.'/../vendor/autoload.php';
 
-try {
-    (new Application())->handleRequest()->send();
-} catch (Exception $e) {
-    return (new JsonResponse(
-        [
-            'exception' => $e->getMessage(), 'file' => $e->getFile(),
-            'line' => $e->getLine(), 'trace' => $e->getTrace()
-        ]
-    ))->send();
-}
+(new Application())->handleRequest()->send();
