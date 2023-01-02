a0cf0d3af87dbba3e01e29c8975f08bd6004246c
Ivan Petrov <lazytechwork@gmail.com>
2023-01-02 18:53:44 +0300

    Added port in database connection

--- src/Application.php
+++ src/Application.php
@@ -69,6 +69,7 @@ class Application
         $capsule->addConnection([
             'driver' => $_ENV['DB_DRIVER'],
             'host' => $_ENV['DB_HOST'],
+            'port' => $_ENV['DB_PORT'],
             'database' => $_ENV['DB_DATABASE'],
             'username' => $_ENV['DB_USERNAME'],
             'password' => $_ENV['DB_PASSWORD'],
