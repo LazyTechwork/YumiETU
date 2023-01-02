c1a475dba5aa395405565441f69ffea2af0f3f27
Ivan Petrov <lazytechwork@gmail.com>
2023-01-02 20:43:36 +0300

    Fix schema getter

--- database/migrations/2023_01_03_025000_create_users_table.php
+++ database/migrations/2023_01_03_025000_create_users_table.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('users', static function (Blueprint $table) {
+        schema()->create('users', static function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('telegram_id')->nullable()->comment(
                 'Telegram ID'
@@ -21,6 +20,6 @@ return new class extends Migration {
 
     public function down(): void
     {
-        Schema::drop('users');
+        schema()->drop('users');
     }
 };
--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,18 @@
 <?php
 
+use Illuminate\Database\Schema\Builder;
 use Yumi\Application;
 
 if (!function_exists('app')) {
     function app(): Application
     {
         return Application::$instance;
+    }
+}
+
+if (!function_exists('schema')) {
+    function schema(): Builder
+    {
+        return app()->getCapsule()->getConnection()->getSchemaBuilder();
     }
 }
