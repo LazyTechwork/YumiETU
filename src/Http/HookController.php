350dd77e57e3b122361e96e98fb4d3ae7d2a2e9e
Ivan Petrov <lazytechwork@gmail.com>
2023-01-02 16:06:57 +0300

    Throwing exception from TG up

--- src/Http/HookController.php
+++ src/Http/HookController.php
@@ -9,15 +9,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HookController implements Controller
 {
+    /**
+     * @throws TelegramException
+     */
     public function __invoke(Request $request): Response
     {
         global $TELEGRAM;
-        try {
-            $TELEGRAM->addCommandsPath(__DIR__.'/../Commands');
+        $TELEGRAM->addCommandsPath(__DIR__.'/../Commands');
 
-            return new JsonResponse(['handled' => $TELEGRAM->handle()]);
-        } catch (TelegramException $e) {
-            return new Response($e->getMessage(), $e->getCode());
-        }
+        return new JsonResponse(['handled' => $TELEGRAM->handle()]);
     }
 }
