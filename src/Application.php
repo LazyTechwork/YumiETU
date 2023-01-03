4b2336f3a47a4b19e950fd760f7fd8e0f4791af9
Ivan Petrov <lazytechwork@gmail.com>
2023-01-04 02:45:14 +0300

    Fixes with encoding

--- src/Application.php
+++ src/Application.php
@@ -76,8 +76,8 @@ class Application
             'database' => $_ENV['DB_DATABASE'],
             'username' => $_ENV['DB_USERNAME'],
             'password' => $_ENV['DB_PASSWORD'],
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
         ]);
         $this->capsule->bootEloquent();
