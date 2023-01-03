7fa0a4a2c76ca31843f2483d937691bfff62b2b7
Ivan Petrov <lazytechwork@gmail.com>
2023-01-03 21:17:13 +0300

    Improved sending messages

--- src/Commands/Marriages/ListCommand.php
+++ src/Commands/Marriages/ListCommand.php
@@ -4,7 +4,6 @@ namespace Yumi\Commands\Marriages;
 
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
-use Longman\TelegramBot\Request;
 use Yumi\Models\Marriage;
 
 class ListCommand extends UserCommand
@@ -24,19 +23,23 @@ class ListCommand extends UserCommand
             'Found '.$marriages->count().' marriages.',
             $marriages->toArray()
         );
-        $result = Request::sendMessage([
-            'chat_id' => $this->getMessage()->getChat()->getId(),
-            'text' => "Список браков:\n".$marriages
-                    ->map(static fn(Marriage $marriage) => sprintf(
-                        '<a href="tg://user?id=%d">%s</a> и <a href="tg://user?id=%d">%s</a> (%d дней)',
-                        $marriage->husband->telegram_id,
-                        $marriage->husband->name,
-                        $marriage->wife->telegram_id,
-                        $marriage->wife->name,
-                        $marriage->daysSinceMarriage
-                    ))->join("\n"),
-            'parse_mode' => 'HTML'
-        ]);
+        $result = $this->replyToChat(
+            "Список браков:\n".$marriages
+                ->map(static fn(Marriage $marriage) => sprintf(
+                    '<a href="tg://user?id=%d">%s</a> и <a href="tg://user?id=%d">%s</a> (%d дней)',
+                    $marriage->husband->telegram_id,
+                    $marriage->husband->name,
+                    $marriage->wife->telegram_id,
+                    $marriage->wife->name,
+                    $marriage->daysSinceMarriage
+                ))->join("\n"),
+            [
+                'chat_id' => $this->getMessage()->getChat()->getId(),
+                'parse_mode' => 'HTML',
+                'disable_notification' => true,
+                'reply_to_message_id' => $this->getMessage()->getMessageId()
+            ]
+        );
         if (!$result->isOk()) {
             logger()->error('Send message failed', $result->getRawData());
         }
