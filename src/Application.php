c827cf1b9518d1f8f1cb2cfd11251f645000d2f4
Ivan Petrov <lazytechwork@gmail.com>
2023-01-04 02:08:59 +0300

    UTF-16

--- src/Application.php
+++ src/Application.php
@@ -77,7 +77,7 @@ class Application
             'username' => $_ENV['DB_USERNAME'],
             'password' => $_ENV['DB_PASSWORD'],
             'charset' => 'utf8',
-            'collation' => 'utf16_unicode_ci',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
         ]);
         $this->capsule->bootEloquent();
