da2e2a9fd10c632b738ff2aa654b3342c308345b
Ivan Petrov <lazytechwork@gmail.com>
2023-01-04 12:36:59 +0300

    Marriage list

--- src/Commands/MeCommand.php
+++ src/Commands/MeCommand.php
@@ -4,6 +4,7 @@ namespace Yumi\Commands;
 
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Yumi\Models\Marriage;
 use Yumi\Models\User;
 
 class MeCommand extends UserCommand
@@ -15,6 +16,12 @@ class MeCommand extends UserCommand
     public function execute(): ServerResponse
     {
         $user = User::createFromCommand($this);
+        $marriages = Marriage::query()
+            ->where('wife_id', $user->id)
+            ->orWhere('husband_id', $user->id)
+            ->orderByDesc('married_since')
+            ->with(['wife', 'husband'])
+            ->get();
 
         return $this->replyToChat(
             sprintf(
@@ -22,13 +29,30 @@ class MeCommand extends UserCommand
                 ."Дата рождения: %s\n"
                 ."Идентификатор: %d\n"
                 ."Идентификатор Telegram: %d\n"
-                ."Идентификатор ВКонтакте: %d",
+                ."Идентификатор ВКонтакте: %s\n"
+                ."Акты смены гражданского состояния:\n%s",
                 $user->telegramMention,
                 $user->birthday ? $user->birthday->format('d.m.Y')
                     : 'не установлена',
                 $user->id,
                 $user->telegram_id,
-                $user->vk_id ?? 'не связан'
+                $user->vk_id ?? 'не связан',
+                $marriages->map(
+                    static fn(Marriage $marriage) => sprintf(
+                        'Брак: %s и %s (рег. %d, %s)',
+                        $marriage->husband->telegramMention,
+                        $marriage->wife->telegramMention,
+                        $marriage->id,
+                        $marriage->divorced_since === null ? sprintf(
+                            '%s&mdash;%s, в браке %d дней',
+                            $marriage->married_since->format('d.m.Y'),
+                            $marriage->divorced_since->format('d.m.Y'),
+                            $marriage->divorced_since->diffInDays(
+                                $marriage->married_since
+                            )
+                        ) : $marriage->daysSinceMarriage
+                    )
+                )->join("\n")
             ), [
                 'reply_to_message_id' => $this->getMessage()->getMessageId(),
                 'parse_mode' => 'HTML'
